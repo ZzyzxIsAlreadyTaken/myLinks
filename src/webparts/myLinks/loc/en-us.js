b47b3d883a2117aa347adb369f7cbff9f6bf71b0
Kjetil <theoz_@hotmail.com>
2023-03-29 13:45:42 +0200

    Added default text property pane properties lists

--- src/webparts/myLinks/loc/en-us.js
+++ src/webparts/myLinks/loc/en-us.js
@@ -1,8 +1,11 @@
+// eslint-disable-next-line no-undef
 define([], function() {
   return {
     "PropertyPaneDescription": "Webdelen lister lenker valgt av enkeltpersoner. Listen i bakkant må inneholde et sett med kolonner og må ha instillingen som gjør at man bare ser sine egne items",
     "BasicGroupName": "Innstillinger for webdel",
     "DescriptionFieldLabel": "Beskrivelse",
+    "MyLinksLabel": "My Links Title",
+    "MyAdminLinksLabel": "My Admin Links Title",
     "AppLocalEnvironmentSharePoint": "The app is running on your local environment as SharePoint web part",
     "AppLocalEnvironmentTeams": "The app is running on your local environment as Microsoft Teams app",
     "AppSharePointEnvironment": "The app is running on SharePoint page",
